c314ae49bfac451a0a425edb525c07fd391c1fe4
HossamMuhammedOmar <hossamomar9876@gmail.com>
2022-03-20 16:58:53 +0200

    input logic

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
-// Refactor the code so that it uses .addEventListener()
-// when you click the SAVE INPUT button
+let myLeads = ["www.awesomelead.com", "www.epiclead.com", "www.greatlead.com"];
+const inputEl = document.getElementById("input-el");
+const inputBtn = document.getElementById("input-btn");
+const ulEl = document.getElementById("ul-el");
 
-function saveLead() {
-    console.log("Button clicked!")
+inputBtn.addEventListener("click", function () {
+  const inputValue = inputEl.value;
+  myLeads.push(inputValue);
+  const li = document.createElement("li");
+  li.textContent = inputValue;
+  if (inputValue.length != 0) {
+    ulEl.appendChild(li);
+    inputEl.value = "";
+  }
+});
+
+for (let i = 0; i < myLeads.length; i++) {
+  console.log(myLeads[i]);
 }
-
-
-
